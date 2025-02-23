aa27cc8a68510fd8e9e80afc8ad36ba97b6dffd6
Ben Eng <whysguy52@gmail.com>
2025-02-22 18:34:18 -0600

    many movement changes
    much better. fine tune next?

--- assets/TestShip/pilot_controller.gd
+++ assets/TestShip/pilot_controller.gd
@@ -23,7 +23,8 @@ var manualPitch = 0
 var manualYaw = 0
 
 #ship mmove/displace
-var speed
+var isForward = false
+var speed = 0.1
 var acc = 0.01
 
 # Called when the node enters the scene tree for the first time.
@@ -33,59 +34,34 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+  manualRoll = int(Input.is_action_pressed("key_e")) - int(Input.is_action_pressed("key_q"))
+  manualPitch = int(Input.is_action_pressed("key_s")) - int(Input.is_action_pressed("key_w"))
+  manualYaw = int(Input.is_action_pressed("key_d")) - int(Input.is_action_pressed("key_a"))
   pass
 
 func _physics_process(delta):
-
+  if !isPilot:
+    return
+    #manual overrides auto turning by putting it first
+  if isTurning:
+    ship_turn()
   if manualRoll != 0:
     manual_roll()
-
+  if manualPitch != 0:
+    manual_pitch()
   if manualYaw != 0:
     manual_yaw()
 
-  if isTurning:
-    ship_turn()
+  if isForward:
+    accelerate_ship()
+
   pass
 
 func _input(event):
-  if !isPilot:
-    return
-
-#TODO: Move the following to LOOKUP: void _unhandled_key_input(event: InputEvent) virtual
-#region Roll, Pitch and Yaw
-  print("...")
-  #roll
-  if  event.is_action_released("key_e") or event.is_action_released("key_q"):
-    print("e/q released")
-    manualRoll = 0
-  if event.is_action_pressed("key_e", true):
-    print("e pressed")
-    manualRoll = 1
-  if event.is_action_pressed("key_q", true):
-    print("q pressed")
-    manualRoll = -1
-
-  #pitch
-  if event.is_action_pressed("key_w"):
-    manualPitch = 1
-  elif event.is_action_pressed("key_s"):
-    manualPitch = -1
-  elif event.is_action_released("key_w") or event.is_action_released("key_s"):
-    manualPitch = 0
-  #yaw
-  if event.is_action_pressed("key_d"):
-    manualYaw = 1
-  elif event.is_action_pressed("key_a"):
-    manualYaw = -1
-  elif event.is_action_released("key_a") or event.is_action_released("key_d"):
-    manualYaw = 0
-#endregion
-
-#TODO: implement acceleration!
   if event.is_action_pressed("key_r"):
-    speed = 1
+    isForward = true
   elif event.is_action_pressed("key_f"):
-    speed = 0
+    isForward = false
 
   if event.is_action_pressed("RMB"):
     rotWeight = 0.0
@@ -94,14 +70,18 @@ func _input(event):
     initialDirection = -shipRotator.transform.basis.z
 
 func manual_roll():
+  isTurning = false
   shipRotator.rotate(-shipRotator.transform.basis.z, manualRoll * manualTurnRate)
-
 func manual_yaw():
+  isTurning = false
   shipRotator.rotate(shipRotator.transform.basis.y, -manualYaw * manualTurnRate)
-
 func manual_pitch():
+  isTurning = false
   shipRotator.rotate(shipRotator.transform.basis.x, manualPitch * manualTurnRate)
 
+func accelerate_ship():
+  global_position += -shipRotator.global_transform.basis.z * speed
+  pass
 
 func ship_turn():
   if rotWeight == 1.0:    #if we are done rotating
