e9db9b4352497058ae212f923b2c28d95452c47e
Ben Eng <whysguy52@gmail.com>
2025-05-03 01:15:35 -0500

    swapped map detection object to player ship and load planets in correct position

--- assets/universal_Scenes/scene_manager.gd
+++ assets/universal_Scenes/scene_manager.gd
@@ -1,21 +1,25 @@
 extends Node3D
 
 var currentScene
+var mapPlayer
 var warpScene
 var planet_scene # make this an array that loads dynamically on ready
 
 func _ready():
   var planetsList = $TestShip/CanvasLayer/HUD/SubViewportContainer/SubViewport/MiniSolarSystem/Planets.get_children()
   currentScene = $CurrentScene
+  mapPlayer = $TestShip/CanvasLayer/HUD/SubViewportContainer/SubViewport/MiniSolarSystem/MapPlayer
   planet_scene = load("res://assets/levels/SolarSystemAssets/scene_planet.tscn")
   warpScene = load("res://assets/TestShip/warp_scene.tscn")
 
-  for planet in planetsList:
-    planet.area_entered.connect(load_planet)
-    planet.area_exited.connect(unload_planet)
+  mapPlayer.area_entered.connect(load_planet)
+  mapPlayer.area_exited.connect(unload_planet)
+  #for planet in planetsList:
+    #planet.area_entered.connect(load_planet)
+    #planet.area_exited.connect(unload_planet)
 
-func unload_planet(map_object):
-  if map_object.name.contains("MapPlayer"):
+func unload_planet(map_object): #needs to load specific planet eventually
+  if map_object.name.contains("MapPlanet"):
     var level = currentScene.get_children()[0]
     currentScene.remove_child(level)
     level.call_deferred("free")
@@ -23,10 +27,11 @@ func unload_planet(map_object):
       currentScene.add_child(warpScene.instantiate())
 
 func load_planet(map_object):
-  if map_object.name.contains("MapPlayer"):
+  if map_object.name.contains("MapPlanet"):
     if !currentScene.get_children().is_empty():
       var level = currentScene.get_children()[0]
       currentScene.remove_child(level)
       level.call_deferred("free")
     currentScene.add_child(planet_scene.instantiate())
     $TestShip.set_near_planet(true)
+    currentScene.get_children()[0].global_position = (map_object.global_position - mapPlayer.global_position) * 1000
