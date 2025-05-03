39a49ead6c85280a1e8446be8e4c641016458243
Ben Eng <whysguy52@gmail.com>
2025-05-03 17:53:57 -0500

    removed 2 unnecessary functions

--- assets/TestShip/screen_layer.gd
+++ assets/TestShip/screen_layer.gd
@@ -13,12 +13,3 @@ func set_planet_references(planets):
     var planetMarker = planetHudIconSrc.instantiate()
     planetMarker.set_my_planet(planet)
     $PlanetHUDList.add_child(planetMarker)
-
-func _process(delta):
-  pass
-  #draw_planet_markers()
-
-func draw_planet_markers(hudPlanets):
-  #make more efficient. don't create new each time
-
-  pass
